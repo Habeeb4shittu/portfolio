c49a8a34ebdbcd6e2db181753b5de120923360ba
Habeeb <habeeb4shittu@gmail.com>
2022-11-06 22:03:32 +0100

    index.html

--- assets/scripts/components/pages/services.js
+++ assets/scripts/components/pages/services.js
@@ -27,35 +27,35 @@ export function servicePage() {
                 <div class="service-card">
                     <div class="body">
                         <div class="title">
-                            <h1>Front-End Development</h1>
+                            <h1>Back-End Development</h1>
                         </div>
                         <div class="divider"></div>
                         <div class="text">
-                            Front-End Development with fully responsive and interactive User-Interface at an affordable Price
+                            Back-End Development with PHP and Node.js at an affordable Price
                         </div>
                         <div class="butt">
                             <button>Order</button>
                         </div>
                     </div>
                     <div class="inter">
-                        <i class="fas fa-globe ico"></i>
+                        <i class="fas fa-bacterium ico"></i>
                     </div>
                 </div>
                 <div class="service-card">
                     <div class="body">
                         <div class="title">
-                            <h1>Front-End Development</h1>
+                            <h1>Database Management</h1>
                         </div>
                         <div class="divider"></div>
                         <div class="text">
-                            Front-End Development with fully responsive and interactive User-Interface at an affordable Price
+                            Database Management With MySQL at an affordable Price
                         </div>
                         <div class="butt">
                             <button>Order</button>
                         </div>
                     </div>
                     <div class="inter">
-                        <i class="fas fa-globe ico"></i>
+                        <i class="fas fa-database ico"></i>
                     </div>
                 </div>
             </div>
